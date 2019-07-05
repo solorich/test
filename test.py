0fedec54b06b335fd461170f6ff2548b0e898361
Chrisitan Solorio <solorich@oregonstate.edu>
2019-07-05 12:53:08 -0700

    Added some files for testing anaconda environments

--- test.py
+++ test.py
@@ -1,5 +1,48 @@
 import nltk
 
-from nltk.book import *
+from nltk.tokenize import sent_tokenize, word_tokenize
 
-print("I'm working", text1, "see?")
+text = open("sample_writing.txt", "r")
+
+with open("sample_writing.txt", "r") as myfile:
+    contents_raw = myfile.read()
+
+contents = str(contents_raw)
+
+tokenized_sent = sent_tokenize(contents)
+
+#print(tokenized_sent)
+
+tokenized_word = word_tokenize(contents)
+
+#print(tokenized_word)
+
+from nltk.probability import FreqDist
+
+fdist = FreqDist(tokenized_word)
+
+print(fdist)
+
+print(fdist.most_common(20))
+
+'''
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words("english"))
+
+filtered_contents = []
+
+for w in tokenized_sent:
+    if w not in stop_words:
+        filtered_contents.append(w)
+
+print("Tokenized contents: ", tokenized_sent)
+
+print("Filtered contents: ", filtered_contents)
+'''
+
+import matplotlib.pyplot as plt
+
+fdist.plot(30, cumulative=False)
+
+plt.show
